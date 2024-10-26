7779787663017e58aaccbb53aae6d1465b180378
PaoloTorregroza <paolinskypdtn@gmail.com>
2024-10-26 18:05:01 -0500

    Mongo authjs setup

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
+import client from '$lib/authDbAdapter';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 
 export const { handle } = SvelteKitAuth({
+	adapter: MongoDBAdapter(client),
 	providers: [GitHub]
 });
--- svelte.config.js
+++ svelte.config.js
@@ -12,4 +12,5 @@ const config = {
 		adapter: adapter()
 	}
 };
+
 export default config;
