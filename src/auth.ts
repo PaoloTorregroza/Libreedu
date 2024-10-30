f887bf8fa6676ab1c9ba0f3655bd219abc04ce73
PaoloTorregroza <paolinskypdtn@gmail.com>
2024-10-30 00:03:14 -0500

    Prisma auth setup, and migrate to mongo atlas database instead of docker one

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,9 @@
-import client from '$lib/authDbAdapter';
-import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import { prisma } from '$lib/prisma';
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/sveltekit/providers/github';
 
 export const { handle } = SvelteKitAuth({
-	adapter: MongoDBAdapter(client),
+	adapter: PrismaAdapter(prisma),
 	providers: [GitHub]
 });
