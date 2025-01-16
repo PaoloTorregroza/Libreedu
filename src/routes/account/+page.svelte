44bcce36e5b06ae8cd032c41d0a6e8d42bb8cb84
PaoloTorregroza <paolinskypdtn@gmail.com>
2025-01-16 16:35:11 -0500

    fix avatar sizing issue

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -17,16 +17,18 @@
 	<div
 		class="visible-card flex w-full max-w-[1410px] flex-col items-center gap-4 p-4 md:w-2/3 md:p-6"
 	>
-		<div class="flex w-full gap-4">
-			<Avatar
-				background="dark:bg-secondary-500 bg-warning-600"
-				rounded="rounded-3xl"
-				size="w-48 h-48"
-				src={userData.image}
-				name={userData.name}
-			/>
+		<div class="flex w-full flex-col items-center gap-4 md:flex-row md:items-start">
+			<div class="min-w-48">
+				<Avatar
+					background="dark:bg-secondary-500 bg-warning-600"
+					rounded="rounded-3xl"
+					size="w-48 h-48"
+					src={userData.image}
+					name={userData.name}
+				/>
+			</div>
 			<div class="flex flex-col gap-4">
-				<h1 class="text-xlg">{userData.name}</h1>
+				<h1 class="text-xlg text-center md:text-start">{userData.name}</h1>
 				<div class="flex gap-3">
 					<div class="variant-ghost-secondary-500 rounded-lg p-2">300 Courses completed</div>
 					<div class="variant-ghost-secondary-500 rounded-lg p-2">240 hours of lessons watched</div>
